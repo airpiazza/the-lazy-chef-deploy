917c1c66e62c1d903fa468f2b1dc10b5f8c3cd9c
airpiazza <nick@piazzaonline.com>
2021-05-12 13:58:27 -0700

    Updates

---
+++ precache-manifest.c69e8e3bed16094804b2ff82b6f0ad31.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d5faec6c65b8255c7abd3f424df0680",
+    "url": "/the-lazy-chef-deploy/index.html"
+  },
+  {
+    "revision": "2b3755961b72fee39228",
+    "url": "/the-lazy-chef-deploy/static/css/main.22d329cf.chunk.css"
+  },
+  {
+    "revision": "47521ca7629397dced64",
+    "url": "/the-lazy-chef-deploy/static/js/2.f736371d.chunk.js"
+  },
+  {
+    "revision": "5438870d5e6f1323093a180d80e38bae",
+    "url": "/the-lazy-chef-deploy/static/js/2.f736371d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2b3755961b72fee39228",
+    "url": "/the-lazy-chef-deploy/static/js/main.c0b956d3.chunk.js"
+  },
+  {
+    "revision": "f314007a5b9db56e6189",
+    "url": "/the-lazy-chef-deploy/static/js/runtime-main.6404c241.js"
+  }
+]);
